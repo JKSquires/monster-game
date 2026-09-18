5225338655a34b05544a217cbd221896d9c0c2b6
Jason K Squires <168765098+JKSquires@users.noreply.github.com>
2026-09-18 14:46:55 -0400

    Base floor collision on tilemap

--- main.asm
+++ main.asm
@@ -6,7 +6,7 @@ b start
 @i "background_tiles.asm"
 
 start:
-mov r0,$4000000
+mov r0,$4000000 ; PERSIST
 
 mov r1,%0001000101000000 ; use BG mode 0, 1D char map, turn on BG0, turn on OBJ screen
 strh r1,[r0]
@@ -30,7 +30,7 @@ str r1,[r0,$DC] ; DMA 3 control ($40000DC)
 adr r1,PlayerIdleSprite
 str r1,[r0,$D4] ; DMA 3 source address ($40000D4)
 
-mov r4,$6000000 ; PERSIST FOR BACKGROUND TILES
+mov r4,$6000000 ; PERSIST
 orr r1,r4,$10000 ; VRAM OBJ char data ($6010000)
 orr r1,r1,$20 ; destination start address: VRAM OBJ char 1 ($6010020)
 str r1,[r0,$D8] ; DMA 3 destination start address ($40000D8)
@@ -73,10 +73,10 @@ strh r1,[r4,r2] ; "
 
 
 ; set up player sprite in OAM
-mov r1,$7000000
+mov r1,$7000000 ; PERSIST
 
 mov r2,%1000000000000000 ; attrib 0: set vertical rectangle shape
-orr r2,r2,#104 ; attrib 0: set y = 160 screen height - 3 * 8 pixels per tile - 32 player sprite height = 104 pixels
+;orr r2,r2,#0 ; initial y-position
 strh r2,[r1] ; OBJ 0 attrib 0 ($7000000)
 
 mov r2,%1000000000000000 ; attrib 1: set size to 16x32 (%10)
@@ -86,12 +86,16 @@ strh r2,[r1,#2] ; OBJ 0 attrib 1 ($7000002)
 mov r2,%0000000000000001 ; attrib 2: use palette 0 and sprite starts at char 1
 strh r2,[r1,#4] ; OBJ 0 attrib 2 ($7000004)
 
-mov r2,#0 ; initial horiz scroll amount and initial y-velocity
+mov r2,#0
 strb r2,[r13,$0] ; horiz scroll amount (IWRAM)
 strb r2,[r13,$1] ; y-velocity
 strb r2,[r13,$2] ; player grounded flag
 
 mainLoop:
+; Persisting Registers:
+; r0: $4000000 (I/O)
+; r1: $7000000 (OAM)
+; r4: $6000000 (VRAM)
 waitForVBlankEnd:
 ldrh r2,[r0,$4] ; LCD status
 tst r2,#1 ; test if inside v-blank interval
@@ -107,12 +111,12 @@ ldrb r5,[r13,$0] ; horiz scroll amount
 ; handle player input
 ldrb r3,[r0,$130] ; key status
 ; handle player jump
-ldrb r4,[r13,$2] ; player grounded flag
-cmp r4,#1 ; check if player is grounded
+ldrb r6,[r13,$2] ; player grounded flag
+cmp r6,#1 ; check if player is grounded
 bne skipJumpInputCheck
 tst r3,%10 ; b
-mvneq r4,#12
-streqb r4,[r13,$1] ; y-vel
+mvneq r6,#12
+streqb r6,[r13,$1] ; y-vel
 skipJumpInputCheck:
 
 ; handle player horizontal movement
@@ -121,8 +125,8 @@ orreq r2,r2,%0001000000000000 ; set OBJ horiz flip flag
 subeq r5,r5,#1
 
 tst r3,%10000 ; d-right
-mvneq r4,%0001000000000000 ; clear OBJ horiz flip flag
-andeq r2,r2,r4 ; "
+mvneq r6,%0001000000000000 ; clear OBJ horiz flip flag
+andeq r2,r2,r6 ; "
 addeq r5,r5,#1
 
 strb r5,[r0,$10] ; BG 0 horiz offset
@@ -130,26 +134,57 @@ strh r2,[r1,#2] ; OBJ 0 (player) attrib 1
 
 strb r5,[r13,$0] ; horiz scroll amount (IWRAM)
 
+
 ; update player y-pos and velocity
 ldrh r2,[r1] ; y-pos (in OBJ 0 attrib 0)
 ldrsb r3,[r13,$1] ; y-vel (signed)
 
-mov r4,r3,asr #2 ; apply only a quarter of velocity to the player (almost like treating velocity as a Q6.2 fixed-point number)
-add r4,r2,r4
-and r4,r4,$FF ; mask only y-pos
+mov r6,r3,asr #2 ; apply only a quarter of velocity to the player (almost like treating velocity as a Q6.2 fixed-point number)
+add r6,r2,r6
+and r6,r6,$FF ; mask only y-pos
 and r2,r2,$FF00 ; mask out y-pos
 
-; check if player is standing on the floor. TODO: might be interesting to base on tilemap rather than hardcoded y-level
-cmp r4,#104 ; floor y = 160 screen height - 3 * 8 pixels per tile - 32 player sprite height = 104 pixels
-orrlt r2,r2,r4
-addlt r3,r3,#1
-movlt r4,#0
+; check if player is standing on the floor
+; calculate tile offset from $6000800 based on horiz scroll (x), y-pos (y), player sprite height (h), and left-right buffer (b). Left-right buffer should be customized to fit the player sprite well so that it looks like they're standing on a ledge when they should be able to (give extra buffer for back so jumps are not as frustrating: "I swear I was standing on the floor!" (was one pixel off)):
+; $40 (bytes per row) * floor((y + h + 1 (adding one to find tile below)) / 8 (pixels per tile)) + $2 * floor(((x + 112 (see player sprite centering) + b) & $FF) / 8 (pixels per tile))
+; = $40 * ((y + h + 1) >> 3) + ((((x + 112 + b) & $FF) >> 3) << 1)
+; = $40 * ((y + h + 1) >> 3) + (((x + 112 + b) & $F8) >> 2)
+; NOTE: calculation can simplified to use fewer registers but will take more instructions (instead of mla, multiply as we get values, then add as we get values, thus only needing to store 3 things at one time rather than 4 but at the cost of one extra instruction). Determine based on need for registers later
+mov r7,$40
+add r8,r6,#33 ; 33 = h (32 pixels tall for player sprite) + 1 pixel
+mov r8,r8,lsr #3
 
-orrge r2,r2,#104 ; "
-movge r3,#0
-movge r4,#1 ; player is grounded
+add r9,r5,#116 ; player left side: 116 = 112 (player sprite centering) + 4 (left buffer)
+and r9,r9,$F8
+mov r9,r9,lsr #2
+mla r9,r7,r8,r9
 
-strb r4,[r13,$2] ; player grounded flag
+add r10,r5,#123 ; player right side: 123 = 112 (player sprite centering) + 11 (right buffer)
+and r10,r10,$F8
+mov r10,r10,lsr #2
+mla r10,r7,r8,r10
+
+add r7,r4,$800 ; tilemap block offset
+ldrh r9,[r7,r9] ; tile directly below player left
+ldrh r10,[r7,r10] ; tile directly below player right
+
+cmp r9,$1 ; check if tile below left is floor tile
+beq floorBelowPlayer
+cmp r10,$1 ; check if tile below right is floor tile
+beq floorBelowPlayer
+noFloorBelowPlayer:
+orr r2,r2,r6
+add r3,r3,#1
+mov r6,#0
+b floorCheckEnd
+floorBelowPlayer:
+and r6,r6,$F8 ; make the player stand flat on the floor (r6 = (r6 & $FF) - (r6 % 8) for r6 >= 0)
+orr r2,r2,r6
+mov r3,#0
+mov r6,#1 ; player is grounded
+floorCheckEnd:
+
+strb r6,[r13,$2] ; player grounded flag
 strb r3,[r13,$1] ; y-vel
 strh r2,[r1] ; y-pos (in OBJ 0 attrib 0)
 
